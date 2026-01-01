b9427bfbdf0d3a4bf82b37c5fbf5917d2bf7e02c
ilmaruk <ruggero.ferretti@gmail.com>
2026-01-01 21:26:05 +0100

    Complete with welcome state, score state and goal carousel

--- sketch.ino
+++ sketch.ino
@@ -12,14 +12,22 @@
 #include "mqtt.h"
 #include "fonts.h"
 
+typedef struct {
+  textEffect_t left;
+  textEffect_t right;
+} WelcomeTransition;
+
 #define ARR_SIZE(arr) ( sizeof((arr)) / sizeof((arr[0])) )
-char *welcomes[6][2] = {
-  {"CIAO", "BENVENUTI"},
-  {"HELLO", "WELCOME"},
-  {"SALUT", "BIENVENUE"},
-  {"HOLA", "BIENVENIDOS"},
-  {"OLA", "BEM-VINDOS"},
-  {"ZDRAVO", "DOBRODOsLI"}
+char *welcome_messages[] = {"BENVENUTI", "BEM-VINDOS", "BIENVENIDOS", "BIENVENUE", // "BONVENON",
+                            "BUN VENItI", "DOBRODOsLI",
+                            // "dObPE dOhln", "LASKAVO PROSIMO",
+                            "TERVETULOA", "uDVoZoLJuK", "VaLKOMNA",
+                            "VELKOMMEN", "ViTEJTE", "WELCOME", "WELKOM", "WILLKOMMEN", "WITAMY"};
+WelcomeTransition welcome_transitions[] = {
+  {PA_SCROLL_DOWN, PA_SCROLL_UP},
+  {PA_DISSOLVE, PA_DISSOLVE},
+  {PA_GROW_DOWN, PA_GROW_UP},
+  {PA_BLINDS, PA_BLINDS},
 };
 
 MD_Parola display = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
@@ -81,16 +89,34 @@ void mqttCallback(char *topic, byte* payload, unsigned int length) {
 
   String event = doc["event"].as<String>();
   if (event == "GOAL_HOME" || event == "GOAL_AWAY") {
-    // displayMode = 2;
+    displayMode = 2;
   }
 }
 
-void update() {
-  if (display.displayAnimate()) {
-  // if (display.getZoneStatus(HOME_BOARD)) {
-    if (displayMode == 1) {
-      // display.displayReset();
+size_t display_until = 0;
+size_t welcome_message_index = 0;
+size_t welcome_transition_index = 0;
 
+void update_display() {
+  if (display.displayAnimate()) {
+    if (displayMode == 0) {
+      if (display_until == 0 || millis() > display_until) {
+        display_until = millis() + 2000;
+        // display.displayClear();
+        display.displayZoneText(HOME_BOARD, welcome_messages[welcome_message_index], PA_CENTER, 100, 3000, welcome_transitions[welcome_transition_index].left, PA_NO_EFFECT);
+        display.displayZoneText(AWAY_BOARD, welcome_messages[welcome_message_index], PA_CENTER, 100, 3000, welcome_transitions[welcome_transition_index].right, PA_NO_EFFECT);
+        welcome_message_index++;
+        if (welcome_message_index >= ARR_SIZE(welcome_messages)) {
+          welcome_message_index = 0;
+        }
+        welcome_transition_index++;
+        if (welcome_transition_index >= ARR_SIZE(welcome_transitions)) {
+          welcome_transition_index = 0;
+        }
+      }
+    } else if (displayMode == 1 && (display_until == 0 || millis() > display_until)) {
+      // Serial.println("displayMode = 1");
+      display_until = 0;
       display.displayZoneText(HOME_NAME, homeName.c_str(), PA_LEFT, 0, 0, PA_NO_EFFECT, PA_NO_EFFECT);
       display.displayZoneText(AWAY_NAME, awayName.c_str(), PA_RIGHT, 0, 0, PA_NO_EFFECT, PA_NO_EFFECT);
       display.displayZoneText(HOME_SCORE, homeScore.c_str(), PA_CENTER, 0, 0, PA_NO_EFFECT, PA_NO_EFFECT);
@@ -99,10 +125,17 @@ void update() {
       // Vertical bar
       // display.getGraphicObject()->setColumn(32, 255);
     } else if (displayMode == 2) {
-      displayMode = 1;
-      Serial.print("A");
-      display.displayZoneText(HOME_BOARD, "GOOOOOL", PA_CENTER, 100, 3000, PA_SCROLL_LEFT, PA_SCROLL_LEFT);
-      Serial.print("B");
+      if (display_until == 0) {
+        display_until = millis() + 8000;
+        display.setInvert(1);
+        display.displayClear();
+        display.displayZoneText(HOME_BOARD, "GOOOOOOOOO", PA_RIGHT, 100, 3000, PA_SCROLL_LEFT, PA_NO_EFFECT);
+        display.displayZoneText(AWAY_BOARD, "OOOOOOOOOL", PA_LEFT, 100, 3000, PA_SCROLL_RIGHT, PA_NO_EFFECT);
+      } else if (millis() > display_until) {
+        display_until = 0;
+        display.setInvert(0);
+        displayMode = 1;
+      }
     }
   }
 }
@@ -133,8 +166,7 @@ void loop() {
 
   mqttClient.loop();
 
-  // Update the display
-  update();
+  update_display();
 
   delay(100);
 }
